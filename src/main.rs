3e8de7adfd920a96ad91f07fd75e6826e9da391c
jglara <jglara@gmail.com>
2024-11-04 16:24:51 +0100

    Add config by file

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ use gpt2_from_scratch::learner::TrainingConfig;
 use gpt2_from_scratch::model::{GPTModel, GPTModelConfig};
 use gpt2_from_scratch::tokenizer::{CharTokenizer, Tokenizer};
 
+use anyhow::Result;
 use clap::Parser;
+use twelf::{config, Layer};
 
 #[derive(Parser)]
 struct Cli {
@@ -22,71 +24,105 @@ struct Cli {
     #[arg(short, long)]
     out: String,
 
-    
+    #[arg(long)]
+    config: String,
+
+    #[arg(long)]
+    generate: usize,
 }
 
-fn main() {
-    let tokenizer = CharTokenizer::new();
+#[config]
+#[derive(Default)]
+struct Conf {
+    steps: usize,
+    batch_size: usize,
+    n_ctxt: usize,
+    learning_rate: f64,
+    n_embd: usize,
+    n_heads: usize,
+    n_layers: usize,
+}
 
-    let config = TrainingConfig {
-        steps: 100,
-        batch_size: 64,
-        block_size: 256,
-        learning_rate: 3e-4,
+fn main() -> Result<()> {
+    let tokenizer = CharTokenizer::new();
+    let cli = Cli::parse();
+
+    let conf = Conf::with_layers(&[Layer::Yaml(cli.config.into())])?;
+
+    let train_config = TrainingConfig {
+        steps: conf.steps,
+        batch_size: conf.batch_size,
+        block_size: conf.n_ctxt,
+        learning_rate: conf.learning_rate,
         optimizer: AdamWConfig::new(),
         model: GPTModelConfig {
             vocab_size: tokenizer.vocab_size(),
-            n_embd: 384,
-            n_heads: 6,
-            n_layers: 6,
-            block_size: 256,
+            n_embd: conf.n_embd,
+            n_heads: conf.n_heads,
+            n_layers: conf.n_layers,
+            block_size: conf.n_ctxt,
         },
         seed: 42,
     };
-
-    let cli = Cli::parse();
 
     if cli.gpu {
         type MyBackend = Autodiff<Wgpu>;
         let device = WgpuDevice::default();
 
         let bm: GPTModel<_> = if cli.train {
-            train::<MyBackend>(&format!("{}", cli.out), &config, "./gpt2_data/shakespeare.txt", device)
+            train::<MyBackend>(
+                &format!("{}", cli.out),
+                &train_config,
+                "./gpt2_data/shakespeare.txt",
+                device,
+            )
         } else {
-            config
+            train_config
                 .model
                 .init(&device)
-                .load_file(&format!("{}/model", cli.out), &CompactRecorder::new(), &device)
-                .expect("File not found")
+                .load_file(
+                    &format!("{}/model", cli.out),
+                    &CompactRecorder::new(),
+                    &device,
+                )?
         };
 
-        let generated = bm.generate(vec![0usize], 100);
+        let generated = bm.generate(vec![0usize], cli.generate);
 
         tokenizer
             .decode(&generated)
             .lines()
             .for_each(|l| println!("{}", l));
-
     } else {
         type MyBackend = Autodiff<NdArray>;
         let device = NdArrayDevice::default();
 
         let bm: GPTModel<_> = if cli.train {
-            train::<MyBackend>(&format!("{}", cli.out), &config, "./gpt2_data/shakespeare.txt", device)
+            train::<MyBackend>(
+                &format!("{}", cli.out),
+                &train_config,
+                "./gpt2_data/shakespeare.txt",
+                device,
+            )
         } else {
-            config
+            train_config
                 .model
                 .init(&device)
-                .load_file(&format!("{}/model", cli.out), &CompactRecorder::new(), &device)
-                .expect("File not found")
+                .load_file(
+                    &format!("{}/model", cli.out),
+                    &CompactRecorder::new(),
+                    &device,
+                )?
+                
         };
-        
-        let generated = bm.generate(vec![0usize], 100);
+
+        let generated = bm.generate(vec![0usize], cli.generate);
 
         tokenizer
             .decode(&generated)
             .lines()
             .for_each(|l| println!("{}", l));
-        
     }
+
+    Ok(())
 }
